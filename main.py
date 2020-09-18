efcf79715cba69f0f9ad1cf61678b66753299bbb
CBrocks294 <68190524+CBrocks294@users.noreply.github.com>
2020-09-18 08:29:13 +0100

    Changed backing wall png to make it larger, added back wall to code, made a main class

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class knight(player):
     def attack(self):
         pass
 
+
 class shield(player):
     def __init__(self):
         super().__init__()
@@ -28,40 +29,62 @@ class shield(player):
     def attack(self):
         pass
 
-def main():
-    pygame.init()
-    screen=pygame.display.set_mode((800,600),pygame.RESIZABLE)
-    done = False
-    clock = pygame.time.Clock()
-
-    Charactors = [knight(), wizard(),shield()]
-
-    CharactorLstPos = 0
-    CurrentCharactor = Charactors[CharactorLstPos]
-
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.VIDEORESIZE:
-                screen=pygame.display.set_mode((event.w,event.h),pygame.RESIZABLE)
-        screen.fill((0,0,0))
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_SPACE]:
-            if Changed == False:
-                CharactorLstPos += 1
-                if CharactorLstPos == 3: CharactorLstPos = 0
-                CurrentCharactor = Charactors[CharactorLstPos]
-                Changed = True  
-        else:
-            Changed = False
-        print(CurrentCharactor.__class__.__name__)
+class backing:
+    def __init__(self):
+        self.BackingImg = pygame.image.load('back brick wall.png').convert()
+        self.BackingImg.set_colorkey((255,255,255))  
+    def drawBacking(self,screen):
         wid = screen.get_width()
         hei = screen.get_height()
-        pygame.draw.rect(screen,(0,255,0),pygame.Rect(100*wid/800,10*hei/600,10*wid/800,100*hei/600))
+        ImgX = int(50*wid/800)
+        ImgY = int(50*hei/600)
+        for x in range(0,wid, ImgX):
+            for y in range(0, hei,ImgY):
+                screen.blit(pygame.transform.scale(self.BackingImg,(ImgX, ImgY)),(x,y))
         pygame.display.update()
-        clock.tick(100)
+        return screen
 
-    pygame.quit()
+class mainClass:
+    def __init__(self):
+        pygame.init()
+        screen=pygame.display.set_mode((800,600),pygame.RESIZABLE)
+        done = False
+        clock = pygame.time.Clock()
+        Charactors = [knight(), wizard(),shield()]
+        CharactorLstPos = 0
+        CurrentCharactor = Charactors[CharactorLstPos]
+        Changed = False
+        Backing = backing()
 
-main()
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+            if event.type == pygame.VIDEORESIZE:
+                screen=pygame.display.set_mode((event.w,event.h),pygame.RESIZABLE)
+           
+            screen = Backing.drawBacking(screen)
+            pressed = pygame.key.get_pressed()
+            if [pygame.K_SPACE]:
+                if Changed == False:
+                    CharactorLstPos += 1
+                    if CharactorLstPos == 3: CharactorLstPos = 0
+                    CurrentCharactor = Charactors[CharactorLstPos]
+                    Changed = True  
+            else:
+                Changed = False
+            print(CurrentCharactor.__class__.__name__)
+            #wid = screen.get_width()
+            #hei = screen.get_height()
+            #pygame.draw.rect(screen,(0,255,0),pygame.Rect(100*wid/800,10*hei/600,10*wid/800,100*hei/600))
+            
+            clock.tick(100)
+            pygame.display.update()
+            screen.fill((0,0,0))
+        pygame.quit()
+
+
+
+Main = mainClass()
+
+
