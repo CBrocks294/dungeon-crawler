0ea75176e0858876593ef6a373f8da085f265cfb
CBrocks294 <68190524+CBrocks294@users.noreply.github.com>
2020-09-18 19:09:12 +0100

    added new backing to increase contract with charactors, started adding the attack for the wizard

--- main.py
+++ main.py
@@ -3,30 +3,46 @@ import pygame, random, time
 class player:
     def __init__(self):
         self.Velocity = [0,0]
-        self.X = 100
         self.Y = 300
-        self.Grounded = False
+        self.Grounded = True
         self.FacingRight = True
+        self.Attacked = False
     def move(self):
         pygame.event.get()
         Pressed = pygame.key.get_pressed()
-        if Pressed[pygame.K_w]:
-            self.Velocity[1] -= 1
+        if Pressed[pygame.K_w] and self.Grounded:
+            self.Velocity[1] -= 10
+            #Grounded = False
         if Pressed[pygame.K_a] and self.Velocity[1] >= -5:
             self.Velocity[0] -= 1
             self.FacingRight = False
         if Pressed[pygame.K_d] and self.Velocity[1] <= 5:
             self.Velocity[0] += 1
             self.FacingRight = True
+        if Pressed[pygame.K_s]:
+            if not self.Attacked:
+                self.attack()
+                self.Attacked = True
+        else: self.Attacked = False
 
-        if self.Velocity[0] > 0:
-            self.Velocity[0] -= 0.5
+    def VeloCalcs(self):
+        if (self.Velocity[0] <= 0.25 and self.Velocity[0]>= -0.25):
+            self.Velocity[0] = 0
+        elif self.Velocity[0] > 0:
+            self.Velocity[0] -= 0.75
         elif self.Velocity[0] < 0:
-            self.Velocity[0] += 0.5
+            self.Velocity[0] += 0.75
+        #self.Velocity = self.Velocity/self.Velocity*-1
+        #gravity
+        #self.Velocity[1] += 2
+        #checks for collitions
+        if (self.X >= 750 and self.Velocity[0] > 0) or (self.X <= -10 and self.Velocity[0] < 0):
+            self.Velocity[0] = 0
 
+        #adds momentum
         self.X += self.Velocity[0]/5
         self.Y += self.Velocity[1]/5
-
+    
 
     def draw(self,screen):
         wid = screen.get_width()
@@ -35,19 +51,39 @@ class player:
         ImgY = int(60*hei/600)
         screen.blit(pygame.transform.scale(pygame.transform.flip(self.CharImg, self.FacingRight,False),(ImgX, ImgY)),(self.X,self.Y))
         
+class wizardSpell():
+    def __init__(self, Right, X, Y):
+        self.X = X
+        self.Y = Y
+        self.MovingRight = Right
+        self.SpellImg = pygame.image.load('wizard.png').convert()
+        self.SpellImg.set_colorkey((255,255,255))  
+    def spellMove(self):
+        if self.MovingRight: self.X += 1
+        else: self.X -= 1
+    def drawSpells(self, wid, hei):
+        self.wid = wid 
+        self.hei = hei
+        ImgX = int(20*self.wid/800)
+        ImgY = int(20*self.hei/600)
+        screen.blit(pygame.transform.scale(pygame.transform.flip(self.SpellImg, self.MovingRight,False),(ImgX, ImgY)),(self.X,self.Y))
 
 
 class wizard(player):
     def __init__(self):
         super().__init__()
+        self.X = 50
         self.CharImg = pygame.image.load('wizard.png').convert()
         self.CharImg.set_colorkey((255,255,255))  
+        self.Attacks = []
 
     def attack(self):
-        pass
+        if len(self.Attacks) <= 5:
+            self.Attacks.append(wizardSpell(self.FacingRight, self.X, self.Y))
 
 class knight(player):
     def __init__(self):
+        self.X = 150
         super().__init__()
         self.CharImg = pygame.image.load('knight.png').convert()
         self.CharImg.set_colorkey((255,255,255))  
@@ -58,6 +94,7 @@ class knight(player):
 class shield(player):
     def __init__(self):
         super().__init__()
+        self.X = 100
         self.CharImg = pygame.image.load('shield.png').convert()
         self.CharImg.set_colorkey((255,255,255))  
     def attack(self):
@@ -65,7 +102,7 @@ class shield(player):
 
 class backing:
     def __init__(self):
-        self.BackingImg = pygame.image.load('back brick wall.png').convert()
+        self.BackingImg = pygame.image.load('new backing wall.png').convert()
         self.BackingImg.set_colorkey((255,255,255))  
     def drawBacking(self,screen):
         wid = screen.get_width()
@@ -97,8 +134,9 @@ class mainClass:
            
             Backing.drawBacking(screen)
             Pressed = pygame.key.get_pressed()
+            wid = screen.get_width()
+            hei = screen.get_height()
             if Pressed[pygame.K_SPACE]:
-                print('space')
                 if Changed == False:
                     CharactorLstPos += 1
                     if CharactorLstPos == 3: CharactorLstPos = 0
@@ -108,10 +146,9 @@ class mainClass:
                 Changed = False
             CurrentCharactor.move()
             for Char in (Charactors):
-                Char.draw(screen)
+                Char.draw(screen, wid, hei)
+                Char.VeloCalcs()
 
-            #wid = screen.get_width()
-            #hei = screen.get_height()
             #pygame.draw.rect(screen,(0,255,0),pygame.Rect(100*wid/800,10*hei/600,10*wid/800,100*hei/600))
             
             clock.tick(100)
