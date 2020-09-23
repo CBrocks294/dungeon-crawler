5d9d4659cc9ec04a72164f1e1455ce4f93add1cc
CBrocks294 <68190524+CBrocks294@users.noreply.github.com>
2020-09-23 09:40:58 +0100

    added spike and lava builder

--- CreateLevels.py
+++ CreateLevels.py
@@ -2,28 +2,39 @@ import pygame, random, time
 class makeLvls():
 	def __init__(self):
 		self.Background = [[0 for y in range(40)] for x in range(30)]
-		self.BlocksImg = pygame.image.load('back brick wall.png').convert()
-		self.BlocksImg.set_colorkey((255,255,255))
+		self.Pngs = []
+		self.Pngs.append(pygame.image.load('back brick wall.png').convert())
+		self.Pngs.append(pygame.image.load('lava.png').convert())
+		self.Pngs.append(pygame.image.load('spikes.png').convert())
+		self.Pngs[2].set_colorkey((255,255,255))  
+		self.PlaceType = 1
 	def placeBlocks(self,screen):
 		pygame.event.get()
 		MousePressed = pygame.mouse.get_pressed()
+		KeyPressed = pygame.key.get_pressed()
+		if KeyPressed[pygame.K_1]:
+			self.PlaceType = 1
+		elif KeyPressed[pygame.K_2]:
+			self.PlaceType = 2
+		if KeyPressed[pygame.K_3]:
+			self.PlaceType = 3
+		print(self.PlaceType)
 		Loc = pygame.mouse.get_pos()
 		wid = screen.get_width()
 		hei = screen.get_height()
+
 		if MousePressed[0]:
-			try:
-				if self.Background[int((Loc[1]/20)/hei*600)][int(Loc[0]/20/wid*800)] == 0:
-					self.Background[int(Loc[1]/20/hei*600)][int(Loc[0]/20/wid*800)] = 1
-					print ('done')
-			except IndexError:
-				print('out of range')
+			if self.Background[int((Loc[1]/20)/hei*600)][int(Loc[0]/20/wid*800)] == 0:
+				self.Background[int(Loc[1]/20/hei*600)][int(Loc[0]/20/wid*800)] = self.PlaceType
+		elif MousePressed[2]:
+			self.Background[int(Loc[1]/20/hei*600)][int(Loc[0]/20/wid*800)] = 0
 	def drawBlocks(self,screen):
 		wid = screen.get_width()
 		hei = screen.get_height()
 		for Y, YLst in enumerate(self.Background):
 			for X, ItemType in enumerate(YLst):
-				if ItemType == 1:
-					screen.blit(pygame.transform.scale(self.BlocksImg,(int(20*wid/800), int(21*hei/600))),(int(X*20*wid/800),int(Y*20*hei/600)))
+				if ItemType != 0:
+					screen.blit(pygame.transform.scale(self.Pngs[ItemType - 1],(int(21*wid/800), int(21*hei/600))),(int(X*20*wid/800),int(Y*20*hei/600)))
 
 class backing:
 	def __init__(self):
